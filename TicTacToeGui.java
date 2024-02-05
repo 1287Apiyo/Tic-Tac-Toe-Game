60f14f43dae1b73566062e839897bd3e991fa95d
Anne Apiyo Opiyo <93118744+1287Apiyo@users.noreply.github.com>
2024-02-05 17:42:41 +0300

    Add files via upload

---
+++ TicTacToeGui.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class TicTacToeGui extends JFrame {
+    public TicTacToeGui(){
+        super("Tic Tac Toe(Java Swing)");
+        setSize(CommonConstraints.FRAME_SIZE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setLayout(null);
+    }
+}
